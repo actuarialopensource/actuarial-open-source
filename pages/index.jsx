e555ff3fa8959206d882e036b7793f4820bebb43
Matthew Caseres <matthewcaseres@outlook.com>
2022-05-27 11:18:36 -0500

    meme seo

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import Hero from "../components/hero";
 import Quotes from "../components/quotes";
 import ProjectGrid from "../components/projectGrid";
 import { getReposFlat, repoUrls } from "../utils/query";
+import { NextSeo } from "next-seo";
 
 export const getStaticProps = async (context) => {
   const repos = await getReposFlat(repoUrls);
@@ -18,12 +19,31 @@ export const getStaticProps = async (context) => {
 
 export default function Home({ repos }) {
   return (
-    <div>
-      <Hero />
-      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-        <ProjectGrid repos={repos} />
-        <Quotes />
+    <>
+      <NextSeo
+        title="Actuarial Open Source"
+        description="Together we can transform actuarial science. A new emphasis on reproducibility by all."
+        canonical="https://actuarialopensource.org/"
+        openGraph={{
+          url: "https://actuarialopensource.org/",
+          title: "Actuarial Open Source",
+          description:
+            "Together we can transform actuarial science. A new emphasis on reproducibility by all.",
+          images: [
+            {
+              url: "https://i.imgflip.com/6hpt7w.jpg",
+              alt: "chad open source vs sad cheems closed source",
+            },
+          ],
+        }}
+      />
+      <div>
+        <Hero />
+        <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+          <ProjectGrid repos={repos} />
+          <Quotes />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
